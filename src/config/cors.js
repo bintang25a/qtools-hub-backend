680b09de7b6d8e8c4943d2336a50cf74c638fc94
bintang25a <bintangalfizar25@gmail.com>
2026-05-30 16:25:34 +0700

    30 Mei 2026

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,6 +4,7 @@ export default cors({
   credentials: true,
   origin: [
     "https://7418fqfm-5173.asse.devtunnels.ms",
+    "https://tjwqh821-5173.asse.devtunnels.ms",
     "https://hqhhcnz8-5173.asse.devtunnels.ms",
     "https://165a-2405-8180-401-7d68-3d58-b19c-b5bd-221b.ngrok-free.app",
     "http://localhost:5173",
